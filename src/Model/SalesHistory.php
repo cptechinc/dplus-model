f4c82ecd2ca170a87d1041961dac6df9218690fc
Paul Gomez <pauldro952@outlook.com>
2019-04-24 11:14:36 -0500

    Update SalesHistory.php
    Co-Authored-By: bbullemer <bbullemer@users.noreply.github.com>

--- src/Model/SalesHistory.php
+++ src/Model/SalesHistory.php
@@ -51,4 +51,160 @@ class SalesHistory extends BaseSalesHistory {
 	public function status() {
 		return self::$status_descriptions[$this->oehhstat];
 	}
+
+	/**
+	 * Filter the query on the ArspSalePer1 column
+	 *
+	 * Example usage:
+	 * <code>
+	 * $query->filterByArspsaleper1('fooValue');   // WHERE ArspSalePer1 = 'fooValue'
+	 * $query->filterByArspsaleper1('%fooValue%', Criteria::LIKE); // WHERE ArspSalePer1 LIKE '%fooValue%'
+	 * </code>
+	 *
+	 * @param     string $arspsaleper1 The value to use as filter.
+	 * @param     string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
+	 *
+	 * @return $this|ChildSalesOrderQuery The current query, for fluid interface
+	 */
+	public function filterbySalesPerson($salesperson = null,  $comparison = null) {
+		$this->condition('sp1', 'SalesHistory.ArspSaleper1 = ? ', $salesperson);
+		$this->condition('sp2', 'SalesHistory.ArspSaleper2 = ? ', $salesperson);
+		$this->condition('sp3', 'SalesHistory.ArspSaleper3 = ? ', $salesperson);
+		$this->where(array('sp1', 'sp2', 'sp3'), 'or');                  // combine 'cond1' and 'cond2' with a logical OR
+		return $this;
+	}
+
+
+	/**
+	 * Return the first SalesHistory filtered by the OehhNbr column
+	 * @param  string       $ordn  Sales Order Number
+	 * @return SalesHistory
+	 */
+	public function findOneByOrderNumber($ordn) {
+		return $this->findOneByOehhnbr($ordn);
+	}
+
+	/**
+	 * Return if Sales Order Exists in so_head_hist
+	 *
+	 * @param  string $ordn Sales Order Number
+	 * @return bool         Does Sales Order Exist
+	 */
+	public function orderExists($ordn) {
+			return boolval($this->filterByOehhnbr($ordn)->count());
+	}
+
+	 /**
+	* Filter the query on the oehhnbr column
+	  *
+	* @param  mixed $ordn   array or string
+	* @return $this|SalesOrderQuery The current query, for fluid interface
+	*/
+	public function filterByOrderNumber($ordn) {
+		if (is_array($ordn)) {
+			if (!empty($ordn[0])) {
+				$this->filterByOehhnbr($ordn[0], Criteria::GREATER_EQUAL);
+			}
+
+			if (!empty($ordn[1])) {
+				$this->filterByOehhnbr($ordn[1], Criteria::LESS_EQUAL);
+			}
+		} else {
+			$this->filterByOehhnbr($ordn);
+		}
+		return $this;
+	}
+
+	/**
+	 * Filter the query on the Arcucustid column
+	 *
+	 * @param  mixed $custid   array or string
+	 * @return $this|SalesOrderQuery The current query, for fluid interface
+	 */
+	public function filterByCustId($custid) {
+		if (is_array($custid)) {
+			if (!empty($custid[0])) {
+				$this->filterByArcucustid($custid[0], Criteria::GREATER_EQUAL);
+			}
+
+			if (!empty($custid[1])) {
+				$this->filterByArcucustid($custid[1], Criteria::LESS_EQUAL);
+			}
+		} else {
+			$this->filterByArcucustid($custid, Criteria::LIKE);
+		}
+		return $this;
+	}
+
+	/**
+	* Filter the query on the Oehhordrtot column
+	*
+	* @param  mixed $ordertotal   array or string
+	* @return $this|SalesOrderQuery The current query, for fluid interface
+	*/
+	public function filterByOrderTotal($ordertotal) {
+		if (is_array($ordertotal)) {
+			if (!empty($ordertotal[0])) {
+				$this->filterByOehhordrtot($ordertotal[0], Criteria::GREATER_EQUAL);
+			}
+
+			if (!empty($ordertotal[1])) {
+				$this->filterByOehhordrtot($ordertotal[1], Criteria::LESS_EQUAL);
+			}
+		}
+		return $this;
+	}
+
+	/**
+	 * Filter the query on the Oehhordrdate column
+	 *
+	 * @param  mixed $orderdate   array or string
+	 * @return $this|SalesOrderQuery The current query, for fluid interface
+	 */
+	public function filterByInvoiceDate($invoicedate) {
+		if (is_array($invoicedate)) {
+			if (!empty($invoicedate[0])) {
+				$this->filterByOehhinvdate($invoicedate[0], Criteria::GREATER_EQUAL);
+			}
+
+			if (!empty($invoicedate[1])) {
+				$this->filterByOehhinvdate($invoicedate[1], Criteria::LESS_EQUAL);
+			}
+		} else {
+			$this->filterByOehhinvdate($invoicedate);
+		}
+		return $this;
+	}
+
+	/**
+	 * Filter the query on the Oehhordrdate column
+	 *
+	 * @param  mixed $orderdate   array or string
+	 * @return $this|SalesOrderQuery The current query, for fluid interface
+	 */
+	 public function filterByOrderDate($orderdate) {
+		 if (is_array($orderdate)) {
+			if (!empty($orderdate[0])) {
+				$this->filterByOehhordrdate($orderdate[0], Criteria::GREATER_EQUAL);
+			}
+
+			if (!empty($orderdate[1])) {
+				$this->filterByOehhordrdate($orderdate[1], Criteria::LESS_EQUAL);
+			}
+		} else {
+			 $this->filterByOehhordrdate($orderdate);
+		}
+		return $this;
+	 }
+
+	/**
+	 * Filter the query on the Oehhstat column
+	 *
+	 * @param  mixed $status   array or string
+	 * @return $this|SalesOrderQuery The current query, for fluid interface
+	 */
+	public function filterByOrderStatus($status) {
+		$this->filterByOehhstat($status);
+		return $this;
+	}
 }
