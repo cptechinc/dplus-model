ba5d40a84606f15dcbece37e0753c915007d8eaf
Paul Gomez <pauldro952@outlook.com>
2019-05-17 10:13:56 -0500

    Update DocumentsQuery.php

--- src/Model/DocumentsQuery.php
+++ src/Model/DocumentsQuery.php
@@ -12,7 +12,41 @@ use Base\DocumentsQuery as BaseDocumentsQuery;
  * long as it does not already exist in the output directory.
  *
  */
-class DocumentsQuery extends BaseDocumentsQuery
-{
+class DocumentsQuery extends BaseDocumentsQuery {
 
+	/**
+	 * Filter by the DociTag column
+	 * @param  string $tag Tag to filter on
+	 * @return DocumentsQuery
+	 */
+	public function filterByTag($tag) {
+		return $this->filterByDocitag($tag);
+	}
+
+	/**
+	 * Filter by the DociFld1 column
+	 * @param  string $ref Tag to filter on
+	 * @return DocumentsQuery
+	 */
+	public function filterByReference1($ref) {
+		return $this->filterByDocifld1($ref);
+	}
+
+	/**
+	 * Filter by the DociFilename column
+	 * @param  string $filename filename to filter on
+	 * @return DocumentsQuery
+	 */
+	public function filterByFilename($filename) {
+		return $this->filterByDocifilename($filename);
+	}
+
+	/**
+	 * Filter by the DoccFolder column
+	 * @param  string $folder folder to filter on
+	 * @return DocumentsQuery
+	 */
+	public function filterByFolder($folder) {
+		return $this->filterByDoccfolder($folder);
+	}
 }
