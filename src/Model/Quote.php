a560c84c0ae796b73a2fe02f1e4f5bdf6b1f94de
Paul Gomez <pauldro952@outlook.com>
2019-09-05 10:19:07 -0500

    column aliases for quote, quotedetail

--- src/Model/Quote.php
+++ src/Model/Quote.php
@@ -2,6 +2,9 @@
 
 use Base\Quote as BaseQuote;
 
+use Dplus\Model\ThrowErrorTrait;
+use Dplus\Model\MagicMethodTraits;
+
 /**
  * Skeleton subclass for representing a row from the 'quote_header' table.
  *
@@ -12,7 +15,61 @@ use Base\Quote as BaseQuote;
  * long as it does not already exist in the output directory.
  *
  */
-class Quote extends BaseQuote
-{
+class Quote extends BaseQuote {
+	use ThrowErrorTrait;
+	use MagicMethodTraits;
 
+	const COLUMN_ALIASES = array(
+		'quotenbr'        => 'qthdid',
+		'quoteid'         => 'qthdid',
+		'quoteID'         => 'qthdid',
+		'id'              => 'qthdid',
+		'quotenumber'     => 'qthdid',
+		'custid'          => 'arcucustid',
+		'shiptoid'        => 'arstshipid',
+		'status'          => 'qthdstat',
+		'email'           => 'qthdemailaddr',
+		'billto_name'     => 'qthdbtname',
+		'billto_address1' => 'qthdbtadr1',
+		'billto_address2' => 'qthdbtadr2',
+		'billto_address3' => 'qthdbtadr3',
+		'billto_country'  => 'qthdbtctry',
+		'billto_city'     => 'qthdbtcity',
+		'billto_state'    => 'qthdbtstat',
+		'billto_zip'      => 'qthdbtzipcode',
+		'shipto_name'     => 'qthdstname',
+		'shipto_address1' => 'qthdstadr1',
+		'shipto_address2' => 'qthdstadr2',
+		'shipto_address3' => 'qthdstadr3',
+		'shipto_country'  => 'qthdstctry',
+		'shipto_city'     => 'qthdstcity',
+		'shipto_state'    => 'qthdststat',
+		'shipto_zip'      => 'qthdstzipcode',
+		'phone_intl'      => 'qthdteleintl',
+		'phone'           => 'qthdtelenbr',
+		'phone_extension' => 'qthdteleext',
+		'fax_intl'        => 'qthdfaxintl',
+		'fax'             => 'qthdfaxnbr',
+		'date_quoted'     => 'qthdquotdate',
+		'date_review'     => 'qthdrevdate',
+		'date_expires'    => 'qthdexpdate',
+		'pricecode'       => 'artbpriccode',
+		'taxcode'         => 'artbmtaxcode',
+		'termscode'       => 'artmtermcd',
+		'shipvia'         => 'artbshipvia',
+		'salesperson_1'   => 'arspsaleper1',
+		'salesperson_2'   => 'arspsaleper2',
+		'salesperson_3'   => 'arspsaleper3',
+		'salesperson_1'   => 'arspsaleper1',
+		'subtotal_nontax' => 'qthdnontaxsub',
+		'subtotal_tax'    => 'qthdtaxsub',
+		'total_total'     => 'qthdordrtot',
+		'total_tax'       => 'qthdtaxtot',
+		'total_freight'   => 'qthdfrttot',
+		'total_misc'      => 'qthdmisctot',
+		'total_cost'      => 'qthdcosttot',
+		'entered_by'      => 'qthdenteredby',
+		'entered_date'    => 'qthdentereddate',
+		'entered_time'    => 'qthdenteredtime',
+	);
 }
--- src/Model/QuoteDetail.php
+++ src/Model/QuoteDetail.php
@@ -2,6 +2,9 @@
 
 use Base\QuoteDetail as BaseQuoteDetail;
 
+use Dplus\Model\ThrowErrorTrait;
+use Dplus\Model\MagicMethodTraits;
+
 /**
  * Skeleton subclass for representing a row from the 'quote_detail' table.
  *
@@ -12,7 +15,36 @@ use Base\QuoteDetail as BaseQuoteDetail;
  * long as it does not already exist in the output directory.
  *
  */
-class QuoteDetail extends BaseQuoteDetail
-{
+class QuoteDetail extends BaseQuoteDetail {
+	use ThrowErrorTrait;
+	use MagicMethodTraits;
 
+	const COLUMN_ALIASES = array(
+		'quotenbr'        => 'qthdid',
+		'quoteid'         => 'qthdid',
+		'quoteID'         => 'qthdid',
+		'id'              => 'qthdid',
+		'quotenumber'     => 'qthdid',
+		'linenbr'         => 'qtdtline',
+		'line'            => 'qtdtline',
+		'itemid'          => 'inititemnbr',
+		'description'     => 'qtdtdesc',
+		'description2'    => 'qtdtdesc2',
+		'whse'            => 'intbwhse',
+		'date_requested'  => 'qtdtrqstdate',
+		'specialorder'    => 'qtdtspecordr',
+		'qty'             => 'qtdtqtyord',
+		'listprice'       => 'qtdtlistpric',
+		'price'           => 'qtdtpric',
+		'price_total'     => 'qtdtprictot',
+		'cost'            => 'qtdtcost',
+		'tax'             => 'qtdttaxpcttot',
+		'tax_code1'       => 'qtdtmstrtaxcode1',
+		'tax_code2'       => 'qtdtmstrtaxcode2',
+		'unitofmeasure'   => 'intbuomsale',
+		'quoted_qty'      => 'qtdtquotunit1',
+		'quoted_price'    => 'qtdtquotpric1',
+		'quoted_cost'     => 'qtdtquotcost1',
+		'quoted_margin'   => 'qtdtquotmkupmarg1',
+	);
 }
