2361b2e84c9c4bf4000adcafc542c0e9fdec79a4
Paul Gomez <pauldro952@outlook.com>
2019-09-19 13:11:34 -0500

    Update SalesHistoryDetail.php

--- src/Model/SalesHistoryDetail.php
+++ src/Model/SalesHistoryDetail.php
@@ -6,7 +6,14 @@ use Dplus\Model\ThrowErrorTrait;
 use Dplus\Model\MagicMethodTraits;
 
 /**
- * Class for representing a row from the 'so_det_hist' table.
+ * Skeleton subclass for representing a row from the 'so_det_hist' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
  */
 class SalesHistoryDetail extends BaseSalesHistoryDetail {
 	use ThrowErrorTrait;
