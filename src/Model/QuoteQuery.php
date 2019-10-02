504f40d40f9170eee43004173e0443540019dc7e
Paul Gomez <pauldro952@outlook.com>
2019-10-02 09:06:34 -0500

    Fixed filterbySalesPerson()

--- src/Model/QuoteQuery.php
+++ src/Model/QuoteQuery.php
@@ -24,4 +24,29 @@ use Dplus\Model\QueryTraits;
  */
 class QuoteQuery extends BaseQuoteQuery {
 	use QueryTraits;
+
+
+	/**
+	 * Filter the query on the ArspSalePer1 column
+	 *
+	 * fix name
+	 *
+	 * Example usage:
+	 * <code>
+	 * $query->filterByArspsaleper1('fooValue');   // WHERE ArspSalePer1 = 'fooValue'
+	 * $query->filterByArspsaleper1('%fooValue%', Criteria::LIKE); // WHERE ArspSalePer1 LIKE '%fooValue%'
+	 * </code>
+	 *
+	 * @param	  string $arspsaleper1 The value to use as filter.
+	 * @param	  string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
+	 *
+	 * @return $this|QuoteQuery The current query, for fluid interface
+	 */
+	public function filterbySalesPerson($salesperson = null) {
+		$this->condition('sp1', 'Quote.ArspSaleper1 = ?', $salesperson);
+		$this->condition('sp2', 'Quote.ArspSaleper2 = ?', $salesperson);
+		$this->condition('sp3', 'Quote.ArspSaleper3 = ?', $salesperson);
+		$this->where(array('sp1', 'sp2', 'sp3'), 'or'); 				 // combine 'cond1' and 'cond2' with a logical OR
+		return $this;
+	 }
 }
