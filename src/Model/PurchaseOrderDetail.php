62c6392c100e6c1ec268254a35f152e44e93988d
Paul Gomez <pauldro952@outlook.com>
2019-10-08 09:48:41 -0500

    Update PurchaseOrderDetail.php

--- src/Model/PurchaseOrderDetail.php
+++ src/Model/PurchaseOrderDetail.php
@@ -7,7 +7,7 @@ use Dplus\Model\MagicMethodTraits;
 
 /**
  * Class for representing a row from the 'po_detail' table.
- * 
+ *
  */
 class PurchaseOrderDetail extends BasePurchaseOrderDetail {
 	use ThrowErrorTrait;
@@ -17,23 +17,24 @@ class PurchaseOrderDetail extends BasePurchaseOrderDetail {
 	 * Column Aliases to lookup / get properties
 	 * @var array
 	 */
-	const COLUMN_ALIASES = array(
-		'ponbr'      => 'pohdnbr',
-		'linenbr'    => 'podtline',
-		'itemid'     => 'inititemnbr',
-		'description' => 'podtdesc1',
-		'desscription2' => 'podtdesc2',
-		'vendoritemid'  => 'podtvenditemnbr',
-		'whse'          => 'inbtwhse',
-		'date_shipped'  => 'podtshipdate',
-		'date_expected' => 'podtexptdate',
-		'uom'           => 'inbuompur',
-		'qty_ordered'   => 'podtqtyord',
-		'cost'          => 'podtcost',
-		'cost_total'    => 'podtcosttot',
-		'specialorder'  => 'podtspecordr',
-		'glaccount'     => 'podtglacct',
-		'weight'        => 'podtwghttot',
-		'whse_destination' => 'podtdestwhse',
-	);
+	 const COLUMN_ALIASES = array(
+ 		'ponbr'      => 'pohdnbr',
+ 		'linenbr'    => 'podtline',
+ 		'itemid'     => 'inititemnbr',
+ 		'description' => 'podtdesc1',
+ 		'desscription2' => 'podtdesc2',
+ 		'vendoritemid'  => 'podtvenditemnbr',
+ 		'whse'          => 'inbtwhse',
+ 		'date_shipped'  => 'podtshipdate',
+ 		'date_expected' => 'podtexptdate',
+ 		'uom'           => 'inbuompur',
+ 		'qty_ordered'   => 'podtqtyord',
+ 		'qty_remaining' => 'podtqtyduein',
+ 		'cost'          => 'podtcost',
+ 		'cost_total'    => 'podtcosttot',
+ 		'specialorder'  => 'podtspecordr',
+ 		'glaccount'     => 'podtglacct',
+ 		'weight'        => 'podtwghttot',
+ 		'whse_destination' => 'podtdestwhse',
+ 	);
 }
