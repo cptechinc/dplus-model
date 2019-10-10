9921e9956fe2a6532d37d56e65e2be85151ca423
Paul Gomez <pauldro952@outlook.com>
2019-10-10 10:22:17 -0500

    PurchaseOrderDetailReceived Model & Query
    Added Traits, COLUMN_ALIASES

---
+++ src/Model/PurchaseOrderDetailReceivedQuery.php
@@ -0,0 +1,23 @@
+<?php
+
+use Base\PurchaseOrderDetailReceivedQuery as BasePurchaseOrderDetailReceivedQuery;
+
+use Dplus\Model\QueryTraits;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'po_receipt_det' table.
+ *
+ * NOTE: you can use the findByXXX(), findOneByXXX(), requireOneByXXX(), filterByXXX(), orderByXXX(), and groupByXXX()
+ * methods with an alias
+ * EXAMPLE: findOneByOrdernumber()
+ *
+ * Magic Methods (NOTE these are the ones in use, not necessarily all the available ones)
+ * -----------------------------------------------------------------------------------------
+ * Filters
+ * @method  PurchaseOrderDetailReceivedQuery filterByPonbr(string $ponbr)       Filter the query on the pohdnbr column
+ * @method  PurchaseOrderDetailReceivedQuery filterByLinenbr(int $Linenbr)      Filter the query on the podtline column
+ *
+ */
+class PurchaseOrderDetailReceivedQuery extends BasePurchaseOrderDetailReceivedQuery {
+	use QueryTraits;
+}
