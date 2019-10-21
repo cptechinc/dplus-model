f1361dd38f2aff3c18056bc1e2f09d198ad4006c
Paul Gomez <pauldro952@outlook.com>
2019-10-21 10:16:17 -0500

    add get_receivedlots()

--- src/Model/PurchaseOrderDetailReceiving.php
+++ src/Model/PurchaseOrderDetailReceiving.php
@@ -7,7 +7,7 @@ use Dplus\Model\MagicMethodTraits;
 
 /**
  * Class for representing a row from the 'po_tran_det' table.
- * 
+ *
  */
 class PurchaseOrderDetailReceiving extends BasePurchaseOrderDetailReceiving {
 	use ThrowErrorTrait;
@@ -18,18 +18,25 @@ class PurchaseOrderDetailReceiving extends BasePurchaseOrderDetailReceiving {
 	 * @var array
 	 */
 	const COLUMN_ALIASES = array(
-		'ponbr'           => 'pohdnbr',
-		'linenbr'         => 'podtline',
+		'ponbr'           => 'pothnbr',
+		'linenbr'         => 'potdline',
 		'itemid'          => 'inititemnbr',
 		'description'     => 'potddesc1',
 		'uom'             => 'intbuompur',
 		'poref'           => 'pordref',
-		'qty_ordered'     => 'pordqtyord',
-		'qty_received'    => 'pordqtyrec',
-		'date_transfered' => 'pordtrandate'
+		'qty_ordered'     => 'potdqtyord',
+		'qty_received'    => 'potdqtyrec',
+		'date_transfered' => 'potdtrandate'
 	);
 
 	public function qty_remaining() {
-		return $this->pordqtyord - $this->pordqtyrec;
+		return $this->potdqtyord - $this->potdqtyrec;
+	}
+
+	public function get_receivedlots() {
+		$q = PurchaseOrderDetailLotReceivingQuery::create();
+		$q->filterByPonbr($this->pothnbr);
+		$q->filterByLinenbr($this->potdline);
+		return $q->find();
 	}
 }
