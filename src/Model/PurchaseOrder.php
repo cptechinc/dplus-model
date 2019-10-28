01d8a60ebcdda3debeb303ad39274ac7a00a77a5
Paul Gomez <pauldro952@outlook.com>
2019-10-28 13:38:30 -0500

    Update PurchaseOrder.php

--- src/Model/PurchaseOrder.php
+++ src/Model/PurchaseOrder.php
@@ -34,12 +34,14 @@ class PurchaseOrder extends BasePurchaseOrder {
 		'contact'            => 'pohdcont',
 		'email'              => 'pohdemailaddr',
 		'date_ordered'       => 'pohdordrdate',
+		'date_expected'      => 'pohdexptdate',
 		'shipvia'            => 'artbsviacode',
 		'phone'              => 'pohdtelenbr',
 		'phone_intl'         => 'pohdteleintl',
 		'phone_extension'    => 'pohdteleext',
 		'fax'                => 'pohdfaxnbr',
 		'fax_intl'           => 'pohdfaxintl',
+		'shipfromid'         => 'apfmshipid',
 		'shipfrom_name'      => 'pohdptname',
 		'shipfrom_address'   => 'pohdptadr1',
 		'shipfrom_address2'  => 'pohdptadr2',
@@ -52,6 +54,11 @@ class PurchaseOrder extends BasePurchaseOrder {
 
 	const LENGTH = 8;
 
+	const STATUS_DESCRIPTIONS = array(
+		'N' => 'new',
+		'C' => 'closed'
+	);
+
 	/**
 	 * Adds Leading Zeroes to Sales Order Number
 	 *
@@ -60,6 +67,15 @@ class PurchaseOrder extends BasePurchaseOrder {
 	 */
 	public static function get_paddedponumber($ordn) {
 		 return str_pad($ordn , self::LENGTH , "0", STR_PAD_LEFT);
+	}
+
+	/**
+	 * Returns Description for the status
+	 *
+	 * @return string
+	 */
+	public function status() {
+		return self::STATUS_DESCRIPTIONS[$this->status];
 	}
 
 	/**
@@ -106,5 +122,17 @@ class PurchaseOrder extends BasePurchaseOrder {
 	 */
 	public function get_vendorname() {
 		return VendorQuery::create()->select(Vendor::get_aliasproperty('name'))->findOneByVendorid($this->apvevendid);
+	}
+
+	/**
+	 *Returns The total for the Purchase Order based off the detail lines
+	 *
+	 * @return float
+	 */
+	public function get_total() {
+		$q = PurchaseOrderDetailQuery::create()->filterByPonbr($this->pohdnbr);
+		$q->withColumn('sum(podtcosttot)', 'totalcost');
+		$q->select('totalcost');
+		return $q->findOne();
 	}
 }
