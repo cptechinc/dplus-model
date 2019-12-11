7163a8e9c5033605c90d6f61d12925076f4b4fb9
bbullemer <barbara.bullemer@gmail.com>
2019-12-11 13:32:37 -0600

    #48
    - added Criteria
    - added functions for filtering of invoice number, vendor id, and date invoiced

--- src/Model/ApInvoiceQuery.php
+++ src/Model/ApInvoiceQuery.php
@@ -1,4 +1,5 @@
 <?php
+use Propel\Runtime\ActiveQuery\Criteria;
 
 use Base\ApInvoiceQuery as BaseApInvoiceQuery;
 
@@ -22,4 +23,68 @@ use Dplus\Model\QueryTraits;
  */
 class ApInvoiceQuery extends BaseApInvoiceQuery {
     use QueryTraits;
+
+    /**
+	* Filter the query on the Oehdnbr column
+	*
+	* @param  mixed $ordn	 array or string
+	* @return $this|SalesOrderQuery The current query, for fluid interface
+	*/
+   public function filterByInvnbr($invnbr) {
+	   if (is_array($invnbr)) {
+		   if (!empty($invnbr[0])) {
+			   $this->filterByApihinvnbr($invnbr[0], Criteria::GREATER_EQUAL);
+		   }
+
+		   if (!empty($invnbr[1])) {
+			   $this->filterByApihinvnbr($invnbr[1], Criteria::LESS_EQUAL);
+		   }
+	   } else {
+		   $this->filterByApihinvnbr($invnbr);
+	   }
+	   return $this;
+   }
+
+   /**
+	* Filter the query on the Arcucustid column
+	*
+	* @param  mixed $vendid   array or string
+	* @return $this|SalesOrderQuery The current query, for fluid interface
+	*/
+   public function filterByVendorid($vendid) {
+	   if (is_array($vendid)) {
+		   if (!empty($vendid[0])) {
+			   $this->filterByApvevendid($vendid[0], Criteria::GREATER_EQUAL);
+		   }
+
+		   if (!empty($vendid[1])) {
+			   $this->filterByApvevendid($vendid[1], Criteria::LESS_EQUAL);
+		   }
+	   } else {
+		   $this->filterByApvevendid($vendid, Criteria::EQUAL);
+	   }
+	   return $this;
+   }
+
+   /**
+	* Filter the query on the Oehdordrdate column
+	*
+	* @param  mixed $orderdate	array or string
+	* @return $this|SalesOrderQuery The current query, for fluid interface
+	*/
+   public function filterByDate_invoiced($invoiceddate) {
+	   if (is_array($invoiceddate)) {
+		   if (!empty($invoiceddate[0])) {
+			   $this->filterByApihinvdate($invoiceddate[0], Criteria::GREATER_EQUAL);
+		   }
+
+		   if (!empty($orderdate[1])) {
+			   $this->filterByApihinvdate($invoiceddate[1], Criteria::LESS_EQUAL);
+		   }
+	   } else {
+		   $this->filterByApihinvdate($invoiceddate);
+	   }
+	   return $this;
+   }
+
 }
