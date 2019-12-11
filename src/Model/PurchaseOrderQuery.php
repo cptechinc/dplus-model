767400e1c8b3c08b89796e035ee0275832714f5c
bbullemer <barbara.bullemer@gmail.com>
2019-12-11 13:31:48 -0600

    #49
    - added Criteria to module
    - added functions for filtering of PONumber, VendorID, date orders, and date expected

--- src/Model/PurchaseOrderQuery.php
+++ src/Model/PurchaseOrderQuery.php
@@ -1,4 +1,5 @@
 <?php
+use Propel\Runtime\ActiveQuery\Criteria;
 
 use Base\PurchaseOrderQuery as BasePurchaseOrderQuery;
 
@@ -6,7 +7,7 @@ use Dplus\Model\QueryTraits;
 
 /**
  * Class for performing query and update operations on the 'po_head' table
- * 
+ *
  * NOTE: you can use the findByXXX(), findOneByXXX(), requireOneByXXX(), filterByXXX(), orderByXXX(), and groupByXXX()
  * methods with an alias
  * EXAMPLE: findOneByPonbr()
@@ -22,4 +23,88 @@ use Dplus\Model\QueryTraits;
  */
 class PurchaseOrderQuery extends BasePurchaseOrderQuery {
 	use QueryTraits;
+
+	/**
+	* Filter the query on the Oehdnbr column
+	*
+	* @param  mixed $ordn	 array or string
+	* @return $this|SalesOrderQuery The current query, for fluid interface
+	*/
+   public function filterByPonbr($ponbr) {
+	   if (is_array($ponbr)) {
+		   if (!empty($ponbr[0])) {
+			   $this->filterByPohdnbr($ponbr[0], Criteria::GREATER_EQUAL);
+		   }
+
+		   if (!empty($ponbr[1])) {
+			   $this->filterByPohdnbr($ponbr[1], Criteria::LESS_EQUAL);
+		   }
+	   } else {
+		   $this->filterByPohdnbr($ponbr);
+	   }
+	   return $this;
+   }
+
+   /**
+	* Filter the query on the Arcucustid column
+	*
+	* @param  mixed $vendid   array or string
+	* @return $this|SalesOrderQuery The current query, for fluid interface
+	*/
+   public function filterByVendorid($vendid) {
+	   if (is_array($vendid)) {
+		   if (!empty($vendid[0])) {
+			   $this->filterByApvevendid($vendid[0], Criteria::GREATER_EQUAL);
+		   }
+
+		   if (!empty($vendid[1])) {
+			   $this->filterByApvevendid($vendid[1], Criteria::LESS_EQUAL);
+		   }
+	   } else {
+		   $this->filterByApvevendid($vendid, Criteria::EQUAL);
+	   }
+	   return $this;
+   }
+
+   /**
+	* Filter the query on the Oehdordrdate column
+	*
+	* @param  mixed $orderdate	array or string
+	* @return $this|SalesOrderQuery The current query, for fluid interface
+	*/
+   public function filterByDate_ordered($orderdate) {
+	   if (is_array($orderdate)) {
+		   if (!empty($orderdate[0])) {
+			   $this->filterByPohdordrdate($orderdate[0], Criteria::GREATER_EQUAL);
+		   }
+
+		   if (!empty($orderdate[1])) {
+			   $this->filterByPohdordrdate($orderdate[1], Criteria::LESS_EQUAL);
+		   }
+	   } else {
+		   $this->filterByPohdordrdate($orderdate);
+	   }
+	   return $this;
+   }
+
+   /**
+	* Filter the query on the Oehdordrdate column
+	*
+	* @param  mixed $orderdate	array or string
+	* @return $this|SalesOrderQuery The current query, for fluid interface
+	*/
+   public function filterByDate_expected($expecteddate) {
+	   if (is_array($expecteddate)) {
+		   if (!empty($expecteddate[0])) {
+			   $this->filterByPohdexptdate($expecteddate[0], Criteria::GREATER_EQUAL);
+		   }
+
+		   if (!empty($orderdate[1])) {
+			   $this->filterByPohdexptdate($expecteddate[1], Criteria::LESS_EQUAL);
+		   }
+	   } else {
+		   $this->filterByPohdexptdate($expecteddate);
+	   }
+	   return $this;
+   }
 }
