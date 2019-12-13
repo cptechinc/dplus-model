dceda996d6eddd6e3606d72e8992c1309a1ee90d
bbullemer <barbara.bullemer@gmail.com>
2019-12-13 11:38:07 -0600

    InvProductLine Model and Query Classes

---
+++ src/Model/InvProductLineCode.php
@@ -0,0 +1,26 @@
+<?php
+
+use Base\InvProductLineCode as BaseInvProductLineCode;
+
+use Dplus\Model\ThrowErrorTrait;
+use Dplus\Model\MagicMethodTraits;
+
+/**
+ * Class for representing a row from the 'inv_plne_code' table.
+ */
+class InvProductLineCode extends BaseInvProductLineCode {
+    use ThrowErrorTrait;
+	use MagicMethodTraits;
+
+	/**
+	 * Column Aliases to lookup / get properties
+	 * @var array
+	 */
+	const COLUMN_ALIASES = array(
+		'id'           => 'intbplnecode',
+		'code'         => 'intbplnecode',
+		'description'  => 'intbplnedesc',
+		'name'         => 'dateupdtd',
+		'email'        => 'timeupdtd'
+	);
+}
