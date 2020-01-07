60fd7e2c23219da9e55fdfbd1578b242a7587467
Paul Gomez <pauldro952@outlook.com>
2020-01-07 13:58:31 -0600

    Update MotorFreightCode.php

--- src/Model/MotorFreightCode.php
+++ src/Model/MotorFreightCode.php
@@ -7,13 +7,6 @@ use Dplus\Model\MagicMethodTraits;
 
 /**
  * Skeleton subclass for representing a row from the 'so_mfrt_code' table.
- *
- *
- *
- * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
- * long as it does not already exist in the output directory.
- *
  */
 class MotorFreightCode extends BaseMotorFreightCode {
 	use ThrowErrorTrait;
