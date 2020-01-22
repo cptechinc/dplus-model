bf359e1fa8a6234c37c742a96c98e41665db7fd4
bbullemer <barbara.bullemer@gmail.com>
2020-01-22 13:32:56 -0600

    #3
    - removed function "filterById", was able to use query class on template and was not needed in class

--- src/Model/TariffCodeCountryQuery.php
+++ src/Model/TariffCodeCountryQuery.php
@@ -8,16 +8,23 @@ use Dplus\Model\QueryTraits;
  * Class for performing query and update operations on the 'inv_trco_code' table.
  */
 class TariffCodeCountryQuery extends BaseTariffCodeCountryQuery {
-    use QueryTraits;
+	use QueryTraits;
 
-    /**
-	 * Filter the query on the Intbtaricode column
-	 *
-	 * @param  mixed $tariffid      string|array
-	 * @return $this|SalesOrderQuery The current query, for fluid interface
-	 */
-	 public function filterById($tariffid) {
-		$this->filterByIntbtaricode($tariffid);
-        return $this->findOne()->country;
+    // used in template - needs replacement
+	public function get_existing_countries($tariffid) {
+		$this->select('intbtrcoctry');
+		$this->filterByCode($tariffid);
+		return $this->find();
 	}
+
+	/**
+	 * Filter the query on the IntbTrcoCtry column
+	 * @param  mixed  $country             The value to use as filter.
+	 * @param  string $comparison           Operator to use for the column comparison, defaults to Criteria::EQUAL
+	 * @return $this|TariffCodeCountryQuery The current query, for fluid interface
+	 */
+	public function filterByCountry($country = null, $comparison = null) {
+		return $this->filterByIntbtrcoctry($intbtrcoctry, $comparison);
+	}
+
 }
