c24389a43b36a684b16b1885bbefb6ae1e2400d1
bbullemer <barbara.bullemer@gmail.com>
2020-01-24 10:53:02 -0600

    Update InvAssortmentCode.php
    - added code for max chracter validation in form

--- src/Model/InvAssortmentCode.php
+++ src/Model/InvAssortmentCode.php
@@ -12,6 +12,8 @@ class InvAssortmentCode extends BaseInvAssortmentCode {
     use ThrowErrorTrait;
     use MagicMethodTraits;
 
+    const MAX_LENGTH_CODE = 3;
+
     /**
      * Column Aliases to lookup / get properties
      * @var array
@@ -23,4 +25,12 @@ class InvAssortmentCode extends BaseInvAssortmentCode {
         'date'         => 'dateupdtd',
         'time'         => 'timeupdtd'
     );
+
+    /**
+	 * Return the Max Number of characters allowed for Code
+	 * @return int
+	 */
+	public function get_max_length_code() {
+		return self::MAX_LENGTH_CODE;
+	}
 }
