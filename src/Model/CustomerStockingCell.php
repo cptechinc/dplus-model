84c094ec332840032c8e4897c40bad504aecf7d6
bbullemer <barbara.bullemer@gmail.com>
2020-01-24 10:43:38 -0600

    Update CustomerStockingCell.php
    - added function for max charcter validation

--- src/Model/CustomerStockingCell.php
+++ src/Model/CustomerStockingCell.php
@@ -12,6 +12,8 @@ class CustomerStockingCell extends BaseCustomerStockingCell {
     use ThrowErrorTrait;
     use MagicMethodTraits;
 
+    const MAX_LENGTH_CODE = 8;
+
     /**
      * Column Aliases to lookup / get properties
      * @var array
@@ -23,4 +25,12 @@ class CustomerStockingCell extends BaseCustomerStockingCell {
         'date'         => 'dateupdtd',
         'time'         => 'timeupdtd'
     );
+
+    /**
+     * Return the Max Number of characters allowed for Code
+     * @return int
+     */
+    public function get_max_length_code() {
+        return self::MAX_LENGTH_CODE;
+    }
 }
