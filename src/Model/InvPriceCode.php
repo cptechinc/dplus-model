af66ab0d6cedf0f8dc1b36e585601e4f763fff4f
bbullemer <barbara.bullemer@gmail.com>
2020-01-24 10:25:54 -0600

    Update InvPriceCode.php
    - added get_max_length_code for chracter validation

--- src/Model/InvPriceCode.php
+++ src/Model/InvPriceCode.php
@@ -9,20 +9,30 @@ use Dplus\Model\MagicMethodTraits;
  * Class for representing a row from the 'inv_pric_code' table.
  */
 class InvPriceCode extends BaseInvPriceCode {
-    use ThrowErrorTrait;
-    use MagicMethodTraits;
+	use ThrowErrorTrait;
+	use MagicMethodTraits;
 
-    /**
-     * Column Aliases to lookup / get properties
-     * @var array
-     */
-    const COLUMN_ALIASES = array(
-        'id'             => 'intbpricgrup',
-        'code'           => 'intbpricgrup',
-        'description'    => 'intbpricdesc',
-        'sales_program'  => 'intbpricsaleprog',
-        'cost_precent'   => 'intbpriccostpct',
-        'date'           => 'dateupdtd',
-        'time'           => 'timeupdtd'
-    );
+	const MAX_LENGTH_CODE = 8;
+
+	/**
+	 * Column Aliases to lookup / get properties
+	 * @var array
+	 */
+	const COLUMN_ALIASES = array(
+		'id'             => 'intbpricgrup',
+		'code'           => 'intbpricgrup',
+		'description'    => 'intbpricdesc',
+		'sales_program'  => 'intbpricsaleprog',
+		'cost_precent'   => 'intbpriccostpct',
+		'date'           => 'dateupdtd',
+		'time'           => 'timeupdtd'
+	);
+
+	/**
+	 * Return the Max Number of characters allowed for Code
+	 * @return int
+	 */
+	public function get_max_length_code() {
+		return self::MAX_LENGTH_CODE;
+	}
 }
