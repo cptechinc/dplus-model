e3a83d428d0ba7895b1fcd24ac306c09fd1324b4
bbullemer <barbara.bullemer@gmail.com>
2020-01-24 10:27:11 -0600

    Update TariffCode.php
    - added functions to get max characters for code for validation
    - added functions to get all countries that code uses, as well as amount

--- src/Model/TariffCode.php
+++ src/Model/TariffCode.php
@@ -9,21 +9,65 @@ use Dplus\Model\MagicMethodTraits;
  * Class for representing a row from the 'inv_tari_code' table.
  */
 class TariffCode extends BaseTariffCode {
-    use ThrowErrorTrait;
+	use ThrowErrorTrait;
 	use MagicMethodTraits;
+
+	const MAX_LENGTH_CODE = 3;
 
 	/**
 	 * Column Aliases to lookup / get properties
 	 * @var array
 	 */
 	const COLUMN_ALIASES = array(
-        'id'           => 'intbtaricode',
-        'code'         => 'intbtaricode',
+		'id'           => 'intbtaricode',
+		'code'         => 'intbtaricode',
 		'number'       => 'intbtarinbr',
 		'description'  => 'intbtaridesc',
 		'duty_rate'    => 'intbtaridutyratepct',
 		'date'         => 'dateupdtd',
-        'time'         => 'timeupdtd'
+		'time'         => 'timeupdtd'
 	);
 
+	/**
+	 * Return the Maximum of characters allowed for code
+	 * @return int
+	 */
+	public function get_max_code_length() {
+		return self::MAX_LENGTH_CODE;
+	}
+
+	/**
+	 * Return Countries that this Tariff Code is applied to
+	 * @return TariffCodeCountry[]|ObjectCollection
+	 */
+	public function get_countries() {
+		return TariffCodeCountryQuery::create()->filterByCode($this->code)->find();
+	}
+
+	/**
+	 * Return the Number of Countries that this Tariff Code is applied to
+	 * @return int
+	 */
+	public function count_countries() {
+		$q = $this->get_query_tariffcountry();
+		return $q->count();
+	}
+
+	/**
+	 * Return if Tariff Code Applies to Country Code
+	 * @param  string $country Country Code
+	 * @return bool
+	 */
+	public function applies_to_country($country) {
+		$q = $this->get_query_tariffcountry();
+		return boolval($q->filterByCountry($country)->count());
+	}
+
+	/**
+	 * Returns Query for Tariff Countries
+	 * @return TariffCodeCountryQuery
+	 */
+	private function get_query_tariffcountry() {
+		return TariffCodeCountryQuery::create()->filterByCode($this->code);
+	}
 }
