cd054a74550ef89ead5eff77d85cfe732fe41405
bbullemer <barbara.bullemer@gmail.com>
2020-01-27 10:38:37 -0600

    Update CustomerPriceCode.php
    - added code to validate the max character allowance for the code field

--- src/Model/CustomerPriceCode.php
+++ src/Model/CustomerPriceCode.php
@@ -19,6 +19,8 @@ class CustomerPriceCode extends BaseCustomerPriceCode {
 	use ThrowErrorTrait;
 	use MagicMethodTraits;
 
+	const MAX_LENGTH_CODE = 4;
+
 	/**
 	 * Column Aliases to lookup / get properties
 	 * @var array
@@ -32,4 +34,12 @@ class CustomerPriceCode extends BaseCustomerPriceCode {
 		'date'		   => 'dateupdtd',
 		'time'		   => 'timeupdtd'
 	);
+
+	/**
+	 * Return the Max Number of characters allowed for Code
+	 * @return int
+	 */
+	public function get_max_length_code() {
+		return self::MAX_LENGTH_CODE;
+	}
 }
