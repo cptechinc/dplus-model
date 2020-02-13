7bad2fd9b9d9671262591d8a24f9eedf4a5ba09f
Paul Gomez <pauldro952@hotmail.com>
2020-02-13 09:32:43 -0600

    ItemMasterItem Documentation
    Noted Foreign Key Relationships

--- src/Model/ItemMasterItem.php
+++ src/Model/ItemMasterItem.php
@@ -6,13 +6,10 @@ use Dplus\Model\ThrowErrorTrait;
 use Dplus\Model\MagicMethodTraits;
 
 /**
- * Skeleton subclass for representing a row from the 'inv_item_mast' table.
+ * Class for representing a row from the 'inv_item_mast' table.
  *
- *
- *
- * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
- * long as it does not already exist in the output directory.
+ * NOTE: Foreign Key Relationship to UnitofMeasurePurchase, UnitofMeasureSale
+ * InvGroupCode, InvCommissionCode, ItemPricing
  *
  */
 class ItemMasterItem extends BaseItemMasterItem {
