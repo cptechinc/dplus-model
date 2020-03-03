b2db93bc2f53ba894890a194b92eaa31b2ed5162
Barbara <barbara@cptechinc.com>
2020-03-03 10:48:45 -0600

    Update MsaSysopCode.php
    - added constant SYSTEM_IN

--- src/Model/MsaSysopCode.php
+++ src/Model/MsaSysopCode.php
@@ -9,37 +9,39 @@ use Dplus\Model\MagicMethodTraits;
  * Class for representing a row from the 'sys_opt_options' table.
  */
 class MsaSysopCode extends BaseMsaSysopCode {
-    use ThrowErrorTrait;
+	use ThrowErrorTrait;
 	use MagicMethodTraits;
 
 	const MAX_LENGTH_CODE = 8;
+
+	const SYSTEM_IN = 'IN';
 
 	/**
 	 * Column Aliases to lookup / get properties
 	 * @var array
 	 */
 	const COLUMN_ALIASES = array(
-        'system'            => 'optnsystem',
+		'system'            => 'optnsystem',
 		'id'                => 'optncode',
 		'code'              => 'optncode',
 		'description'       => 'optndesc',
-        'validate'          => 'optnvalidate',
-        'force'             => 'optnforce',
-        'note_code'         => 'optnnotecode',
-        'list_seq'          => 'optnlistseq',
-        'file_name'         => 'optnfilename',
-        'adv_search'        => 'optnadvsrch',
-        'field_type'        => 'optnfieldtype',
-        'before_dec'        => 'optndef1b4dec',
-        'after_dec'         => 'optndef2aftdec',
-        'doc_store_folder'  => 'optndocstorfolder',
-        'web_validate'      => 'optnwebvalidate',
-        'web_force'         => 'optnwebforce',
+		'validate'          => 'optnvalidate',
+		'force'             => 'optnforce',
+		'note_code'         => 'optnnotecode',
+		'list_seq'          => 'optnlistseq',
+		'file_name'         => 'optnfilename',
+		'adv_search'        => 'optnadvsrch',
+		'field_type'        => 'optnfieldtype',
+		'before_dec'        => 'optndef1b4dec',
+		'after_dec'         => 'optndef2aftdec',
+		'doc_store_folder'  => 'optndocstorfolder',
+		'web_validate'      => 'optnwebvalidate',
+		'web_force'         => 'optnwebforce',
 		'date'		        => 'dateupdtd',
 		'time'		        => 'timeupdtd'
 	);
 
-    /**
+	/**
 	 * Return the Max Number of characters allowed for Code
 	 * @return int
 	 */
