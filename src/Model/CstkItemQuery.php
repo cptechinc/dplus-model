83d202cd20d583266dcc52dc40f4a2241324dd87
Paul Gomez <pauldro952@hotmail.com>
2020-03-05 16:01:12 -0600

    Update CstkItemQuery.php

--- src/Model/CstkItemQuery.php
+++ src/Model/CstkItemQuery.php
@@ -6,11 +6,11 @@ use Dplus\Model\QueryTraits;
 
 /**
  * Class for performing query and update operations on the 'cust_stock_det' table.
- * 
+ *
  * NOTE: you can use the findByXXX(), findOneByXXX(), requireOneByXXX(), filterByXXX(), orderByXXX(), and groupByXXX()
  * methods with an alias
  * EXAMPLE: filterByCustid()
- * 
+ *
  * Magic Methods (NOTE these are the ones in use, not necessarily all the available ones)
  * -----------------------------------------------------------------------------------------
  * FilterByXXX()
