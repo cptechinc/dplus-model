60052b671ed6356437959a76b4f232b02a2685ba
Barbara <barbara@cptechinc.com>
2020-03-13 18:16:10 -0500

    Update ApTypeCode.php

--- src/Model/ApTypeCode.php
+++ src/Model/ApTypeCode.php
@@ -13,12 +13,27 @@ class ApTypeCode extends BaseApTypeCode {
 	use ThrowErrorTrait;
 	use MagicMethodTraits;
 
+	const MAX_LENGTH_CODE = 4;
+
 	/**
 	 * Column Aliases to lookup / get properties
 	 * @var array
 	 */
 	const COLUMN_ALIASES = array(
 		'code'         => 'aptbtypecode',
-		'description'  => 'aptbtypedesc'
+		'description'  => 'aptbtypedesc',
+		'fabricator'   => 'aptbtypefab',
+		'production'   => 'aptbtypeprod',
+		'competitor'   => 'aptbtypecomp',
+		'date'		   => 'dateupdtd',
+		'time'		   => 'timeupdtd'
 	);
+
+	/**
+	 * Return the Max Number of characters allowed for Code
+	 * @return int
+	 */
+	public function get_max_length_code() {
+		return self::MAX_LENGTH_CODE;
+	}
 }
