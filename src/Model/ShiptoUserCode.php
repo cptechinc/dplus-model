355c8c2f12991c176d3916278da68b563df2ca94
Barbara <barbara@cptechinc.com>
2020-03-14 18:37:40 -0500

    ShiptoUserCode Model and Class Files

---
+++ src/Model/ShiptoUserCode.php
@@ -0,0 +1,36 @@
+<?php
+
+use Base\ShiptoUserCode as BaseShiptoUserCode;
+
+use Dplus\Model\ThrowErrorTrait;
+use Dplus\Model\MagicMethodTraits;
+
+/**
+ * Class for representing a row from the 'ar_cust_susr' table.
+ */
+class ShiptoUserCode extends BaseShiptoUserCode {
+    use ThrowErrorTrait;
+	use MagicMethodTraits;
+
+	const MAX_LENGTH_CODE = 4;
+
+	/**
+	 * Column Aliases to lookup / get properties
+	 * @var array
+	 */
+	const COLUMN_ALIASES = array(
+		'id'           => 'artbsusrcode',
+		'code'         => 'artbsusrcode',
+		'description'  => 'artbsusrdesc',
+		'date'		   => 'dateupdtd',
+		'time'		   => 'timeupdtd'
+	);
+
+	/**
+	 * Return the Max Number of characters allowed for Code
+	 * @return int
+	 */
+	public function get_max_length_code() {
+		return self::MAX_LENGTH_CODE;
+	}
+}
