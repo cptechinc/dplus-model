2fe79a67cca03e25bfbf4a8b0b23e08621646409
Barbara <barbara@cptechinc.com>
2020-03-14 18:37:02 -0500

    CustomerUserCode Model and Query Classes

---
+++ src/Model/CustomerUserCode.php
@@ -0,0 +1,36 @@
+<?php
+
+use Base\CustomerUserCode as BaseCustomerUserCode;
+
+use Dplus\Model\ThrowErrorTrait;
+use Dplus\Model\MagicMethodTraits;
+
+/**
+ * Class for representing a row from the 'ar_cust_cusr' table.
+ */
+class CustomerUserCode extends BaseCustomerUserCode {
+    use ThrowErrorTrait;
+	use MagicMethodTraits;
+
+	const MAX_LENGTH_CODE = 4;
+
+	/**
+	 * Column Aliases to lookup / get properties
+	 * @var array
+	 */
+	const COLUMN_ALIASES = array(
+		'id'           => 'artbcusrcode',
+		'code'         => 'artbcusrcode',
+		'description'  => 'artbcusrdesc',
+		'date'		   => 'dateupdtd',
+		'time'		   => 'timeupdtd'
+	);
+
+	/**
+	 * Return the Max Number of characters allowed for Code
+	 * @return int
+	 */
+	public function get_max_length_code() {
+		return self::MAX_LENGTH_CODE;
+	}
+}
