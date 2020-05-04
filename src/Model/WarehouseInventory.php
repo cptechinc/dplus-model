998d307d298f8693e3741f868b1b175bc72ef3ec
Paul Gomez <pauldro952@hotmail.com>
2020-05-04 16:13:36 -0500

    Update WarehouseInventory.php

--- src/Model/WarehouseInventory.php
+++ src/Model/WarehouseInventory.php
@@ -14,6 +14,11 @@ class WarehouseInventory extends BaseWarehouseInventory {
 
 	const STATUS_ACTIVE   = 'A';
 	const STATUS_INACTIVE = 'I';
+	const STATUS_DELETE   = 'D';
+
+	const SPECIALORDER_SPECIAL  = 'S';
+	const SPECIALORDER_NORMAL   = 'N';
+	const SPECIALORDER_DROPSHIP = 'D';
 
 	/**
 	 * Column Aliases to lookup / get properties
@@ -26,9 +31,15 @@ class WarehouseInventory extends BaseWarehouseInventory {
 		'warehouseID'  => 'intbwhse',
 		'warehouseid'  => 'intbwhse',
 		'bin_default'  => 'inwhbin',
-		'status'       => 'inwhstat'
+		'status'       => 'inwhstat',
+		'orderpoint'   => 'inwhordrpnt',
+		'orderqty'     => 'inwhordrqty',
+		'maxqty'       => 'inwhmax',
+		'countdate'    => 'inwhcntdate',
+		'whsesupply'   => 'inwhsupplywhse',
+		'specialorder' => 'inwhspecordr'
 	);
-	
+
 	/**
 	 * Returns if Warehouse Item is active
 	 *
@@ -36,5 +47,14 @@ class WarehouseInventory extends BaseWarehouseInventory {
 	 */
 	public function is_active() {
 		return $this->status == self::STATUS_ACTIVE;
+	}
+
+	/**
+	 * Returns if Warehouse Item is a special Order
+	 *
+	 * @return bool
+	 */
+	public function is_specialorder() {
+		return $this->specialorder == self::SPECIALORDER_SPECIAL;
 	}
 }
