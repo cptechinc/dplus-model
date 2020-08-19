8f4648d297ac3a4398182fb10e87cd893352f234
Paul Gomez <pauldro952@hotmail.com>
2020-08-19 16:19:13 -0500

    Update PurchaseOrderNoteInternal.php

--- src/Model/PurchaseOrderNoteInternal.php
+++ src/Model/PurchaseOrderNoteInternal.php
@@ -14,6 +14,9 @@ class PurchaseOrderNoteInternal extends BasePurchaseOrderNoteInternal {
 
 	const TYPE = 'INTL';
 	const DESC = 'Internal Purchase Order Notes';
+	const CONTROL_NBR = '00000000';
+	const FORMAT_NOTEDATE = 'Ymd';
+	const FORMAT_NOTETIME = 'His';
 
 	/**
 	 * Column Aliases to lookup / get properties
@@ -29,11 +32,44 @@ class PurchaseOrderNoteInternal extends BasePurchaseOrderNoteInternal {
 		'type'        => 'ponttype',
 		'description' => 'ponttypedesc',
 		'key2'        => 'pontkey2',
-		'user'        => 'pontuser',
-		'notedate'    => 'pointldate',
-		'notetime'    => 'pointltime',
+		'userid'      => 'pontintluser',
+		'notedate'    => 'pontintldate',
+		'notetime'    => 'pontintltime',
 		'date'        => 'dateupdtd',
 		'time'        => 'timeupdtd'
 	);
-	
+
+	/**
+	 * Return Note Time in His Format
+	 * @return string
+	 */
+	public function notetime() {
+		return substr($this->notetime, 0, 6);
+	}
+
+	/**
+	 * Sets generated Key2
+	 * NOTE: Key2 = ItemID followed by the Key2 Append
+	 *
+	 * @return string
+	 */
+	public function generateKey2() {
+		$ponbr = PurchaseOrder::get_paddedponumber($this->ponbr);
+		$linenbr = str_pad($this->linenbr , 4 , "0", STR_PAD_LEFT);
+		$key2 = $ponbr.self::CONTROL_NBR.$linenbr.$this->notedate.$this->notetime.$this->userid;
+		$this->setKey2($key2);
+	}
+
+	/**
+	 * Return new PurchaseOrderNote
+	 *
+	 * @return PurchaseOrderNote
+	 */
+	public static function new() {
+		$note = new PurchaseOrderNoteInternal();
+		$note->setType(self::TYPE);
+		$note->setDescription(self::DESC);
+		$note->setForm('');
+		return $note;
+	}
 }
