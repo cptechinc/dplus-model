6a8e48c293219274e211fcfcc21f1253c35e7450
Paul Gomez <pauldro952@hotmail.com>
2020-12-28 13:21:04 -0600

    Update ItemPricingDiscount.php

--- src/Model/ItemPricingDiscount.php
+++ src/Model/ItemPricingDiscount.php
@@ -18,6 +18,9 @@ class ItemPricingDiscount extends BaseItemPricingDiscount {
 	const TYPE_PRICEDISCOUNT = 'PD';
 	const TABLE_CUSTID_ITEMID = 3;
 
+	const UNITS_AVAILABLE = 5;
+	const METHOD_S = 'S';
+
 	/**
 	 * Column Aliases to lookup / get properties
 	 * @var array
@@ -32,6 +35,67 @@ class ItemPricingDiscount extends BaseItemPricingDiscount {
 		'itemgroupcode' => 'oepcitemgrup',
 		'salespersonid' => 'oepcsp',
 		'baseprice' => 'oepcpricbase',
-		
+		'lastchangedate' => 'oepclastchgdate',
+		'standardcost'   => 'oepcstancost',
+		'contcost'       => 'oepccontcost',
+		'enddate'        => 'oepcenddate',
+		'percent'        => 'oepcpcnt',
+		'date'                  => 'dateupdtd',
+		'time'                  => 'timeupdtd',
+		'method'                => 'oepcmeth',
+		'qtybreak'              => 'oepcqtybrk'
 	);
+
+	public function price($x = 0) {
+		if ($x > 0 && $x < self::UNITS_AVAILABLE + 1) {
+			$base = 'oepcpricprice';
+			$col = $base.$x;
+			return $this->$col;
+		} else {
+			return false;
+		}
+	}
+
+	public function priceunit($x = 0) {
+		if ($x > 0 && $x < self::UNITS_AVAILABLE + 1) {
+			$base = 'oepcpricunit';
+			$col = $base.$x;
+			return $this->$col;
+		} else {
+			return false;
+		}
+	}
+
+	public function setPrice($x = 0, $val) {
+		if ($x > 0 && $x < self::UNITS_AVAILABLE + 1) {
+			$base = 'oepcpricpric';
+			$col = $base.$x;
+			$func = "set".ucfirst($col);
+			return $this->$func($val);
+		}
+	}
+
+	public function setPriceunit($x = 0, $val) {
+		if ($x > 0 && $x < self::UNITS_AVAILABLE + 1) {
+			$base = 'oepcpricunit';
+			$col = $base.$x;
+			$func = "set".ucfirst($col);
+			return $this->$func($val);
+		}
+	}
+
+	public function setDefaults() {
+		$this->setPercent('0.0000000');
+		$this->setContcost('0.000');
+		$this->setStandardcost('0.0000000');
+		$this->setEnddate('00000000');
+		$this->setMethod('S');
+		$this->setQtybreak(0);
+		
+		for ($i = 1; $i < self::UNITS_AVAILABLE + 1; $i++) {
+			$this->setPrice($i, '0.0000000');
+			$this->setPriceunit($i, 0);
+		}
+	}
+
 }
