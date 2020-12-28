2e59bbe98c0ba77b2d13c0238071b0c00f8a9c62
Paul Gomez <pauldro952@hotmail.com>
2020-12-28 13:21:24 -0600

    Update OptionsIi.php

--- src/Model/OptionsIi.php
+++ src/Model/OptionsIi.php
@@ -35,6 +35,86 @@ class OptionsIi extends BaseOptionsIi {
 	 */
 	const COLUMN_ALIASES = array(
 		'user'              => 'iitboptncode',
-		'view_requirements' => 'iitboptnreqrview'
+		'userid'            => 'iitboptncode',
+		'view_requirements' => 'iitboptnreqrview',
+		// II Screen Permissions
+		'activity'          => 'IitbOptnActAvail',
+		'cost'              => 'IitbOptnCostAvail',
+		'general'           => 'IitbOptnGenAvail',
+		'kit'               => 'IitbOptnKtAvail',
+		'pricing'           => 'IitbOptnPricAvail',
+		'purchasehistory'   => 'IitbOptnPhAvail',
+		'purchaseorders'    => 'IitbOptnPoAvail',
+		'requirements'      => 'IitbOptnReqrAvail',
+		'saleshistory'      => 'IitbOptnShAvail',
+		'salesorders'       => 'IitbOptnSoAvail',
+		'lotserial'         => 'IitbOptnSerlotAvail',
+		'stock'             => 'IitbOptnStckAvail',
+		'substitutes'       => 'IitbOptnSubsupAvail',
+		'lostsales'         => 'IitbOptnLsAvail',
+		// II Screen Warehouse Options
+		'whseactivity'         => 'IitbOptnActWhse',
+		'whsecost'             => 'IitbOptnCostWhse',
+		'whsepurchasehistory'  => 'IitbOptnPhWhse',
+		'whsepurchaseorders'   => 'IitbOptnPoWhse',
+		'whserequirement'      => 'IitbOptnReqrWhse',
+		'whsesaleshistory'     => 'IitbOptnShWhse',
+		'whsesalesorders'      => 'IitbOptnSoWhse',
+		'whsestock'            => 'IitbOptnStckWhse',
+		'whsesubstitutes'      => 'IitbOptnSubsupWhse',
+		'whselostsales'        => 'IitbOptnLsWhse',
+
+		// II Screen Date Options
+		'daysactivity'         => 'IitbOptnActDaysBack',
+		'dateactivity'         => 'IitbOptnActStrtDate',
+		'dayspurchasehistory'  => 'IitbOptnPhDaysBack',
+		'datepurchasehistory'  => 'IitbOptnPhStrtDate',
+		'dayssaleshistory'  => 'IitbOptnShDaysBack',
+		'datesaleshistory'  => 'IitbOptnShStrtDate',
+
+		// II Screen Detail Options
+		'detailactivity'        => 'IitbOptnActDet',
+		'detailcost'            => 'IitbOptnCostDet',
+		'detailpurchasehistory' => 'IitbOptnPhDet',
+		'detailsaleshistory'    => 'IitbOptnShDet',
+		'detailstock'           => 'IitbOptnStckdet',
+		'detailrequirements'    => 'IitbOptnReqrView'
 	);
+
+	const PERMISSIONS = [
+		'activity',
+		'cost',
+		'general',
+		'kit',
+		'pricing',
+		'purchasehistory',
+		'purchaseorders',
+		'requirements',
+		'saleshistory',
+		'salesorders',
+		'lotserial',
+		'stock',
+		'substitute',
+		'lostsales',
+	];
+
+	/**
+	 * Return if Column Value equals the True value
+	 * @param  string $col  Column or Alias
+	 * @return bool
+	 */
+	public function is_true($col) {
+		if (isset($this->$col)) {
+			return $this->$col == self::YN_TRUE;
+		}
+		return false;
+	}
+
+	/**
+	 * Return Array of Permitted Functions
+	 * @return array
+	 */
+	public function permitted() {
+		return array_filter(self::PERMISSIONS, array($this, 'is_true'));
+	}
 }
