2afa49e0697a8d33f301cf0c2c0616f6659e3587
Paul Gomez <pauldro952@hotmail.com>
2020-12-29 12:05:31 -0600

    Update OptionsCi.php

--- src/Model/OptionsCi.php
+++ src/Model/OptionsCi.php
@@ -13,13 +13,66 @@ class OptionsCi extends BaseOptionsCi {
 	use ThrowErrorTrait;
 	use MagicMethodTraits;
 
+	const YN_TRUE = 'Y';
+
 	/**
 	 * Column Aliases to lookup / get properties
 	 * @var array
 	 */
 	const COLUMN_ALIASES = array(
 		'user'     => 'citboptncode',
+		'userid'   => 'citboptncode',
 		'days_po'  => 'citboptnpodaysback',
-		'days_sh'  => 'citboptnshdaysback'
+		'days_sh'  => 'citboptnshdaysback',
+		// CI screens
+		'notes'          => 'CitbOptnNoteAvail',
+		'contacts'       => 'CitbOptnGenAvail',
+		'payment'        => 'CitbOptnPayAvail',
+		'corebank'       => 'CitbOptnCoreAvail',
+		'credit'         => 'CitbOptnCredAvail',
+		'stock'          => 'CitbOptnCstkAvail',
+		'pricing'        => 'CitbOptnPricAvail',
+		'standingorders' => 'CitbOptnStndAvail',
+		'salesorders'    => 'CitbOptnSoAvail',
+		'quotes'         => 'CitbOptnQuotAvail',
+		'openinvoices'   => 'CitbOptnOpenAvail',
+		'customerpo'     => 'CitbOptnPoAvail',
+		'saleshistory'   => 'CitbOptnShAvail'
 	);
+
+	const PERMISSIONS = [
+		'notes',
+		'contacts',
+		'payment',
+		'corebank',
+		'credit',
+		'stock',
+		'pricing',
+		'standingorders',
+		'salesorders',
+		'quotes',
+		'openinvoices',
+		'customerpo',
+		'saleshistory'
+	];
+
+	/**
+	 * Return if Column Value equals the True value
+	 * @param  string $col  Column or Alias
+	 * @return bool
+	 */
+	public function is_true($col) {
+		if (isset($this->$col)) {
+			return $this->$col == self::YN_TRUE;
+		}
+		return false;
+	}
+
+	/**
+	 * Return Array of Permitted Functions
+	 * @return array
+	 */
+	public function permitted() {
+		return array_filter(self::PERMISSIONS, array($this, 'is_true'));
+	}
 }
