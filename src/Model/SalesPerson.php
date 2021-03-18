687fc412e93419a07e60daa59825995020d3a690
Paul <pauldro952@outlook.com>
2021-03-18 09:14:10 -0500

    Update SalesPerson.php

--- src/Model/SalesPerson.php
+++ src/Model/SalesPerson.php
@@ -19,17 +19,57 @@ class SalesPerson extends BaseSalesPerson {
 	 * @var array
 	 */
 	const COLUMN_ALIASES = array(
-		'id'        => 'arspsaleper1',
-		'name'      => 'arspname',
-		'userid'    => 'arsplogin',
-		'manager'   => 'arspmgr',
-		'vendorid'  => 'arspvendid',
-		'email'     => 'arspemailaddr',
+		'id'           => 'arspsaleper1',
+		'name'         => 'arspname',
+		'userid'       => 'arsplogin',
+		'manager'      => 'arspmgr',
+		'vendorid'     => 'arspvendid',
+		'email'        => 'arspemailaddr',
+		'lastsaledate' => 'arsplastsaledate',
+		'groupid'      => 'arspgrup',
+		'cycle'        => 'arspcommcycle',
+		'salesmtd'  => 'arspmtdsale',
+		'salesytd'  => 'arspytdsale',
+		'salesltd'  => 'arspltdsale',
+		'earnedmtd' => 'arspmtdcommearn',
+		'earnedytd' => 'arspytdcommearn',
+		'earnedltd' => 'arspltdcommearn',
+		'paidmtd'   => 'arspmtdcommpaid',
+		'paidytd'   => 'arspytdcommpaid',
+		'paidltd'   => 'arspltdcommpaid',
+		'restricted' => 'arsprestrictaccess',
 		'date'      => 'dateupdtd',
 		'time'      => 'timeupdtd',
 	);
 
+	/**
+	 * Return if User is a Manager
+	 * @return bool
+	 */
 	public function is_manager() {
 		return $this->manager == 'Y';
+	}
+
+	public static function new() {
+		$p = new SalesPerson();
+		$emptystring = ['cycle','groupid','email','vendorid','name','lastsaledate'];
+		$types       = ['sales','earned','paid'];
+		$periods     = ['mtd','ytd','ltd'];
+
+		foreach ($emptystring as $col) {
+			$setCol = 'set'.ucfirst($col);
+			$p->$setCol('');
+		}
+
+		foreach($types as $col) {
+			foreach($periods as $period) {
+				$setCol = 'set'.ucfirst($col).$period;
+				$p->$setCol(0);
+			}
+			
+		}
+		$p->setManager('N');
+		$p->setRestricted('N');
+		return $p;
 	}
 }
