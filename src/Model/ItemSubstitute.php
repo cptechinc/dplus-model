1860e9046666ce14d5167a5f94e339419f068fa6
Paul <pauldro952@outlook.com>
2021-09-24 15:06:31 -0500

    Update ItemSubstitute.php

--- src/Model/ItemSubstitute.php
+++ src/Model/ItemSubstitute.php
@@ -7,12 +7,17 @@ use Dplus\Model\MagicMethodTraits;
 
 /**
  * Class for representing a row from the 'inv_inv_sub' table.
- * 
- * RELATIONSHIPS: ItemMasterItem 
+ *
+ * RELATIONSHIPS: ItemMasterItem
  */
 class ItemSubstitute extends BaseItemSubstitute {
 	use ThrowErrorTrait;
 	use MagicMethodTraits;
+
+	const OPTIONS_SAMEORLIKE = [
+		'S' => 'same',
+		'L' => 'like'
+	];
 
 	/**
 	 * Column Aliases to lookup / get properties
@@ -22,9 +27,11 @@ class ItemSubstitute extends BaseItemSubstitute {
 		'itemid'	   => 'inititemnbr',
 		'subitemid'    => 'insisubitemnbr',
 		'subsituteitemid'	 => 'insisubitemnbr',
+		'sameOrLike'   => 'insisamelike',
 		'date'		   => 'dateupdtd',
 		'time'		   => 'timeupdtd',
 	);
+
 
 	/**
 	 * Get the associated ItemMasterItem object for the Original Item
@@ -32,11 +39,19 @@ class ItemSubstitute extends BaseItemSubstitute {
 	public function getItem() {
 		return $this->getItemMasterItemRelatedByInititemnbr();
 	}
-	
+
 	/**
 	 * Get the associated ItemMasterItem object for the Subtitute Item
 	 */
 	public function getSubstitute() {
 		return $this->getItemMasterItemRelatedByInsisubitemnbr();
+	}
+
+	/**
+	 * Return Same Or Like Description
+	 * @return string
+	 */
+	public function sameOrLikeDescription() {
+		return self::OPTIONS_SAMEORLIKE[$this->sameOrLike];
 	}
 }
