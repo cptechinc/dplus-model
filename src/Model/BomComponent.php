749bcd258bca068a7fd216afd7f210213c3f54c2
Paul <pauldro952@outlook.com>
2021-10-08 15:42:54 -0500

    Update BomComponent.php

--- src/Model/BomComponent.php
+++ src/Model/BomComponent.php
@@ -7,7 +7,7 @@ use Dplus\Model\MagicMethodTraits;
 
 /**
  * Class for representing a row from the 'pr_bmat_det' table.
- * 
+ *
  * PURPOSE: BoM Component
  * RELATIONSHIPS: ItemMasterItem, BomItem
  */
@@ -20,11 +20,33 @@ class BomComponent extends BaseBomComponent {
 	 * @var array
 	 */
 	const COLUMN_ALIASES = array(
-		'itemid'      => 'bomduseagitem',
+		'itemid'      => 'bomdusagitem',
 		'line'        => 'bomdline',
 		'produces'    => 'bomhproditem',
 		'qty'         => 'bomdusagqty',
+		'usageqty'    => 'bomdusagqty',
 		'scrap'       => 'bomdscrap',
 		'serialbase'  => 'bomdserialbase',
+		'item'        => 'itm',
+		'date'        => 'dateupdtd',
+		'time'        => 'timeupdtd',
 	);
+
+	/**
+	 * Return ItemMasterItem
+	 * @return ItemMasterItem
+	 */
+	public function getItm() {
+		return ItemMasterItemQuery::create()->findOneByItemid($this->itemid);
+	}
+
+	/**
+	 * Return if Component is a Serialized Item
+	 * @return bool
+	 */
+	public function isSerialized() {
+		$q = ItemMasterItemQuery::create()->filterByItemid($this->itemid);
+		$q->filterByItemtype(ItemMasterItem::ITEMTYPE_SERIALIZED);
+		return boolval($q->count());
+	}
 }
