266437627ab6e9192a43d2d2185c1aba20289fae
Paul <pauldro952@outlook.com>
2021-10-08 15:43:47 -0500

    Update ConfigPm.php

--- src/Model/ConfigPm.php
+++ src/Model/ConfigPm.php
@@ -11,4 +11,35 @@ use Dplus\Model\MagicMethodTraits;
 class ConfigPm extends BaseConfigPm {
 	use ThrowErrorTrait;
 	use MagicMethodTraits;
+
+	const YN_TRUE = 'Y';
+
+	/**
+	 * Column Aliases to lookup / get properties
+	 * @var array
+	 */
+	const COLUMN_ALIASES = array(
+		'key'            => 'pmtbconfkey',
+		'scrapUnused'    => 'pmtbconfscrapunused',
+		'useSerialBase'  => 'pmtbconfserialbase',
+
+		'date'           => 'dateupdtd',
+		'time'           => 'timeupdtd',
+	);
+
+	/**
+	 * Return if ScrapUnused is True
+	 * @return bool
+	 */
+	public function scrapUnused() {
+		return strtoupper($this->scrapUnused) == self::YN_TRUE;
+	}
+
+	/**
+	 * Return if SerialBase is True
+	 * @return bool
+	 */
+	public function useSerialBase() {
+		return strtoupper($this->useSerialBase) == self::YN_TRUE;
+	}
 }
