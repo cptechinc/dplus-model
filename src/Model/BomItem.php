1b93da6d15582071fa59861fc75280f7715e7bbf
Paul <pauldro952@outlook.com>
2021-10-21 16:20:45 -0500

    Update BomItem.php

--- src/Model/BomItem.php
+++ src/Model/BomItem.php
@@ -7,7 +7,7 @@ use Dplus\Model\MagicMethodTraits;
 
 /**
  * Class for representing a row from the 'pr_bmat_head' table.
- * 
+ *
  * PURPOSE: BoM Finished Good
  * RELATIONSHIPS: ItemMasterItem
  */
@@ -22,5 +22,27 @@ class BomItem extends BaseBomItem {
 	const COLUMN_ALIASES = array(
 		'itemid'  => 'bomhproditem',
 		'level'   => 'bomhlevel',
+		'date'    => 'dateupdtd',
+		'time'    => 'timeupdtd',
 	);
+
+	/**
+	 * Return the number of Sub Components
+	 * @return int
+	 */
+	public function countComponents() {
+		$q = BomComponentQuery::create();
+		$q->filterByProduces($this->itemid);
+		return $q->count();
+	}
+
+	/**
+	 * Return Sub Components
+	 * @return BomComponent[]
+	 */
+	public function getComponents() {
+		$q = BomComponentQuery::create();
+		$q->filterByProduces($this->itemid);
+		return $q->find();
+	}
 }
