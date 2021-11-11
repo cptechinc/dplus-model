65a31f9d6cc5e28f413c645199f67636afb8ea19
Paul <pauldro952@outlook.com>
2021-11-11 08:59:08 -0600

    Update InvOptCodeNote.php

--- src/Model/InvOptCodeNote.php
+++ src/Model/InvOptCodeNote.php
@@ -7,7 +7,7 @@ use Dplus\Model\MagicMethodTraits;
 
 /**
  * Class for representing a row from the 'notes_opt_code_in' table.
- * 
+ *
  * RELATIONSHIPS: ItemMasterItem
  */
 class InvOptCodeNote extends BaseInvOptCodeNote {
@@ -33,5 +33,25 @@ class InvOptCodeNote extends BaseInvOptCodeNote {
 		'time'         => 'timeupdtd',
 		'item'         => 'itemMasterItem'
 	);
+
+	/**
+	 * Set Key 2
+	 * @return void
+	 */
+	public function generateKey2() {
+		$key = [$this->itemid];
+		$key2 = implode('', $key);
+		$this->setKey2($key2);
+	}
+
+	/**
+	 * Return New Instance with some defaults set
+	 * @return self
+	 */
+	public static function new() {
+		$c = new self();
+		$c->setSystem(self::SYSTEM);
+		return $c;
+	}
 
 }
