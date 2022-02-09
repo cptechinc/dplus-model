016b19681085fd1591340530d23b5ac9dbe97945
Paul <pauldro952@outlook.com>
2022-02-09 08:56:27 -0600

    Update InvWhseLotQuery.php

--- src/Model/InvWhseLotQuery.php
+++ src/Model/InvWhseLotQuery.php
@@ -1,14 +1,25 @@
 <?php
-
-use Base\InvWhseLotQuery as BaseInvWhseLotQuery;
-
+// Dplus Model
 use Dplus\Model\QueryTraits;
+// Base Classes
+use Base\InvWhseLotQuery as BaseInvWhseLotQuery;
 
 /**
  * Class for performing query and update operations on the 'inv_inv_lot' table.
  */
 class InvWhseLotQuery extends BaseInvWhseLotQuery {
 	use QueryTraits;
+
+	/**
+	 * Filter the query on the InltBin column
+	 * @param     string $binID      The value to use as filter.
+	 * @param     string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
+	 *
+	 * @return $this|ChildInvLotMasterQuery The current query, for fluid interface
+	 */
+	public function filterByBinid($binID, $comparison = null) {
+		return $this->filterByInltbin($binID, $comparison);
+	}
 
 	/**
 	 * Filter the query on the InitItemNbr column
