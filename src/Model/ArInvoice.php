860e03cdd0bcd3f811b9b34b791576b1f14ef198
Paul <pauldro952@outlook.com>
2022-02-16 11:29:04 -0600

    ArInvoice

--- src/Model/ArInvoice.php
+++ src/Model/ArInvoice.php
@@ -7,46 +7,6 @@ use Dplus\Model\MagicMethodTraits;
 
 /**
  * Class for representing a row from the 'ar_inv' table.
- * 
- * FKRELATIONSHIPS: Customer
- */
-class ArInvoice extends BaseArInvoice {
-	use ThrowErrorTrait;
-	use MagicMethodTraits;
-
-	const TYPE_MEMO    = 'M';
-	const TYPE_PAYMENT = 'M';
-	const TYPE_INVOICE = 'I';
-
-	private $arPayment;
-
-	/**
-	 * Column Aliases to lookup / get properties
-	 * @var array
-	 */
-	const COLUMN_ALIASES = array(
-		'invoicenumber'  => 'arininvnbr',
-		'invnbr'         => 'arininvnbr',
-		'custid'         => 'arcucustid',
-		'type'           => 'arintype',
-		'invoicedate'    => 'arininvdate',
-		'duedate'        => 'arinduedate',
-		'custpo'         => 'arincustpo',
-		'checknbr'       => 'arinchknbr',
-		'total'          => 'arintotamt',
-		'date'           => 'dateupdtd',
-		'time'           => 'timeupdtd',
-	);
-
-	<?php
-
-use Base\ArInvoice as BaseArInvoice;
-
-use Dplus\Model\ThrowErrorTrait;
-use Dplus\Model\MagicMethodTraits;
-
-/**
- * Class for representing a row from the 'ar_inv' table.
  *
  * FKRELATIONSHIPS: Customer
  */
@@ -96,6 +56,5 @@ class ArInvoice extends BaseArInvoice {
 	public function hasPaymentPending() {
 		return boolval(ArPaymentPendingQuery::create()->filterByInvoicenbr($this->invnbr)->count());
 	}
-}
 
 }
