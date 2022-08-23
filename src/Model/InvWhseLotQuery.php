1879a3cba00f523020e153f78c135ed2851e7e3b
Paul <pauldro952@outlook.com>
2022-08-23 10:21:31 -0500

    add filterByLotserial()

--- src/Model/InvWhseLotQuery.php
+++ src/Model/InvWhseLotQuery.php
@@ -12,8 +12,8 @@ class InvWhseLotQuery extends BaseInvWhseLotQuery {
 
 	/**
 	 * Filter the query on the InltBin column
-	 * @param     string $binID      The value to use as filter.
-	 * @param     string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
+	 * @param	  string $binID 	 The value to use as filter.
+	 * @param	  string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
 	 *
 	 * @return $this|ChildInvLotMasterQuery The current query, for fluid interface
 	 */
@@ -23,8 +23,8 @@ class InvWhseLotQuery extends BaseInvWhseLotQuery {
 
 	/**
 	 * Filter the query on the InitItemNbr column
-	 * @param     string $inititemnbr The value to use as filter.
-	 * @param     string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
+	 * @param	  string $inititemnbr The value to use as filter.
+	 * @param	  string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
 	 *
 	 * @return $this|ChildInvLotMasterQuery The current query, for fluid interface
 	 */
@@ -35,16 +35,34 @@ class InvWhseLotQuery extends BaseInvWhseLotQuery {
 	/**
 	 * Filter the query on the InltOnHand column
 	 *
-	 * @param     mixed $inltonhand The value to use as filter.
-	 *              Use scalar values for equality.
-	 *              Use array values for in_array() equivalent.
-	 *              Use associative array('min' => $minValue, 'max' => $maxValue) for intervals.
-	 * @param     string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
+	 * @param	  mixed $inltonhand The value to use as filter.
+	 *				Use scalar values for equality.
+	 *				Use array values for in_array() equivalent.
+	 *				Use associative array('min' => $minValue, 'max' => $maxValue) for intervals.
+	 * @param	  string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
 	 *
 	 * @return $this|ChildInvLotMasterQuery The current query, for fluid interface
 	 */
 
 	public function filterByQty($qty, $comparison = null) {
 		return $this->filterByInltonhand($qty, $comparison);
+	}
+
+	/**
+	 * Filter the query on the InltLotSer column
+	 *
+	 * Example usage:
+	 * <code>
+	 * $query->filterByInltlotser('fooValue');	 // WHERE InltLotSer = 'fooValue'
+	 * $query->filterByInltlotser('%fooValue%', Criteria::LIKE); // WHERE InltLotSer LIKE '%fooValue%'
+	 * </code>
+	 *
+	 * @param	  string $inltlotser The value to use as filter.
+	 * @param	  string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
+	 *
+	 * @return $this|ChildInvWhseLotQuery The current query, for fluid interface
+	 */
+	public function filterByLotserial($inltlotser = null, $comparison = null) {
+		return $this->filterByInltlotser($inltlotser, $comparison);
 	}
 }
