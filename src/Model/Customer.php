b93d98434b2b26a8945eaafbd63ee543e78eb06f
Paul <pauldro952@outlook.com>
2022-11-09 09:10:30 -0600

    Revert "Update customer classes"
    This reverts commit f49b522378a521ce7644deee75eee56c7f7a0c46.

--- src/Model/Customer.php
+++ src/Model/Customer.php
@@ -32,7 +32,6 @@ class Customer extends BaseCustomer {
 	 * @var array
 	 */
 	const COLUMN_ALIASES = array(
-		'rid'          => 'custrecnbr',
 		'custID'       => 'arcucustid',
 		'id'           => 'arcucustid',
 		'custid'       => 'arcucustid',
