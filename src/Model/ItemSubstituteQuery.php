db54a4d732856333b8d714ac2beaa14c2ac79fcf
Paul <pauldro952@outlook.com>
2023-03-24 14:03:17 -0500

    FEAT: add filterByItemid

--- src/Model/ItemSubstituteQuery.php
+++ src/Model/ItemSubstituteQuery.php
@@ -23,4 +23,22 @@ use Dplus\Model\QueryTraits;
  */
 class ItemSubstituteQuery extends BaseItemSubstituteQuery {
 	use QueryTraits;
+
+	/**
+     * Filter the query on the itemid column
+     *
+     * Example usage:
+     * <code>
+     * $query->filterByInititemnbr('fooValue');   // WHERE InitItemNbr = 'fooValue'
+     * $query->filterByInititemnbr('%fooValue%', Criteria::LIKE); // WHERE InitItemNbr LIKE '%fooValue%'
+     * </code>
+     *
+     * @param     string $inititemnbr The value to use as filter.
+     * @param     string $comparison Operator to use for the column comparison, defaults to Criteria::EQUAL
+     *
+     * @return $this|ChildItemSubstituteQuery The current query, for fluid interface
+     */
+	public function filterByItemid($itemID, $comparison = null) {
+		return $this->filterByInititemnbr($itemID, $comparison);
+	}
 }
