386f9063649f8f8e12aad52908f1f9e2c9a5ede6
Paul <pauldro952@outlook.com>
2023-05-09 09:19:11 -0500

    FIX: add traits

--- src/Model/PoReceivingHead.php
+++ src/Model/PoReceivingHead.php
@@ -2,12 +2,18 @@
 
 use Base\PoReceivingHead as BasePoReceivingHead;
 
+use Dplus\Model\ThrowErrorTrait;
+use Dplus\Model\MagicMethodTraits;
+
 /**
  * Class for representing a row from the 'po_tran_head' table.
  *
  * FKRELATIONSHIPS: PurchaseOrder, Warehouse
  */
 class PoReceivingHead extends BasePoReceivingHead {
+	use ThrowErrorTrait;
+	use MagicMethodTraits;
+
 	/**
 	 * Column Aliases to lookup / get properties
 	 * @var array
