bcc9aa5345c681d35eb9c6edb194b963512348df
Paul Gomez <pauldro952@outlook.com>
2026-04-02 11:37:42 -0500

    FIX: allow rcydtrcptline to be 0 when fetching detail

--- src/Model/RcyclReceiptLot.php
+++ src/Model/RcyclReceiptLot.php
@@ -52,4 +52,26 @@ class RcyclReceiptLot extends BaseRcyclReceiptLot {
         'itmitem'   => 'ItemMasterItem',
         'rcptitem'  => 'RcyclReceiptDetail',
     ];
+
+    /**
+     * Get the associated RcyclReceiptDetail object
+     *
+     * @param  ConnectionInterface $con Optional Connection object.
+     * @return RcyclReceiptDetail The associated RcyclReceiptDetail object.
+     * @throws PropelException
+     */
+    public function getRcyclReceiptDetail(ConnectionInterface $con = null)
+    {
+        if ($this->aRcyclReceiptDetail === null && ($this->rcyhdcntrlnbr != 0 && ($this->rcyhdrcptbulk !== "" && $this->rcyhdrcptbulk !== null) && ($this->inititemnbr !== "" && $this->inititemnbr !== null))) {
+            $this->aRcyclReceiptDetail = RcyclReceiptDetailQuery::create()->findPk(array($this->rcyhdrcptbulk, $this->rcyhdcntrlnbr, $this->rcydtrcptline, $this->inititemnbr), $con);
+            /* The following can be used additionally to
+                guarantee the related object contains a reference
+                to this object.  This level of coupling may, however, be
+                undesirable since it could result in an only partially populated collection
+                in the referenced object.
+                $this->aRcyclReceiptDetail->addRcyclReceiptLots($this);
+             */
+        }
+        return $this->aRcyclReceiptDetail;
+    }
 }
